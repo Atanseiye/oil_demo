0beaa1bcbd7ed855354f939f29dc2d41cd77deff
Kolade <koladeatanseiye@gmail.com>
2025-02-06 02:54:44 +0100

    testing testing

--- testing.py
+++ testing.py
@@ -8,16 +8,18 @@ st.title("Multi-Select Example with Deselect Option")
 uploaded_file = st.file_uploader("Choose files", type=["xlsx"])
 if uploaded_file is not None:
     data = pd.read_excel(uploaded_file)
+    # Use the Feature Engineering function here and save it to the data variable
+    data = feature_engineer(data)
+    # write the head of the data to the front-end
+    st.write(data.head())
     # List of items to select from
-    st.write(feature_engineer(data).head())
     items = data.columns
-    # Multi-select widget
+    # Multi-select widget 
     selected_items = st.multiselect("Select items:", items)
-    st.write(f'---> {type(selected_items)}')
 
-    # Display selected items
+    # Display selected itemsd
     if selected_items:
-        st.write("You selected:", selected_items)
+        st.write(data[selected_items])
     else:
         st.write("No items selected.")
 
